67a76475d35b199a6f77f2a0d9ae404b677d4e45
Himanshu Ahirwar <hiimanshu028@gmail.com>
2024-12-27 06:19:04 +0530

    v0.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About"
+import Experience from "./components/Experience"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Technologies from "./components/Technologies"
@@ -16,6 +17,7 @@ const App = () => {
       <Hero />
       <About />
       <Technologies />
+      <Experience />
       </div>
     </div>
   )
